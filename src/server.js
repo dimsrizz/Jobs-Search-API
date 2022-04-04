546696820f16be5d6ad504cde3b9aeb3dd35771a
Dimas R <dimasrizky943@gmail.com>
2022-04-04 07:21:44 +0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const app = require("./app");
+const connectDB = require("./db/connect");
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
